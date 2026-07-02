9d12948dfe0e4281b22d37d5e505e4644de321e5
Himanshu Nandal <himanshunandal0103@gmail.com>
2026-07-02 10:31:33 +0530

    Time: 484 ms (71.22%), Space: 0B (100%) - LeetHub

---
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select w2.id as Id
+from weather w1 
+inner join weather w2 
+where datediff(w2.recordDate, w1.recordDate) = 1
+and w2.temperature > w1.temperature;
