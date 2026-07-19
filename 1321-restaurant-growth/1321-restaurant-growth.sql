fd1870cd49b5c05380058367954a0e548da91fe8
Himanshu Nandal <himanshunandal0103@gmail.com>
2026-07-19 11:29:14 +0530

    Time: 360 ms (69.84%), Space: 0B (100%) - LeetHub

---
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -0,0 +1,27 @@
+# Write your MySQL query statement below
+WITH DailyRevenue AS
+(
+    SELECT
+        visited_on,
+        SUM(amount) AS amount
+    FROM Customer
+    GROUP BY visited_on
+)
+
+SELECT
+    visited_on,
+    amount,
+    ROUND(amount / 7, 2) AS average_amount
+FROM
+(
+    SELECT
+        visited_on,
+        SUM(amount) OVER(
+            ORDER BY visited_on
+            ROWS BETWEEN 6 PRECEDING
+            AND CURRENT ROW
+        ) AS amount,
+        ROW_NUMBER() OVER(ORDER BY visited_on) AS rn
+    FROM DailyRevenue
+) t
+WHERE rn >= 7;
