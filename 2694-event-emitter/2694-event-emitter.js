d0aa1059b8ff472ddae6ada4f228614e17819062
Himanshu Nandal <himanshunandal0103@gmail.com>
2026-07-28 13:07:10 +0530

    Time: 44 ms (65.86%), Space: 54.5 MB (20.49%) - LeetHub

---
+++ 2694-event-emitter/2694-event-emitter.js
@@ -0,0 +1,44 @@
+class EventEmitter {
+    constructor() {
+        this.events = new Map();
+    }
+
+    subscribe(eventName, callback) {
+        if (!this.events.has(eventName)) {
+            this.events.set(eventName, []);
+        }
+
+        const callbacks = this.events.get(eventName);
+        callbacks.push(callback);
+
+        return {
+            unsubscribe: () => {
+                const index = callbacks.indexOf(callback);
+                if (index !== -1) {
+                    callbacks.splice(index, 1);
+                }
+            }
+        };
+    }
+
+    emit(eventName, args = []) {
+        if (!this.events.has(eventName)) {
+            return [];
+        }
+
+        return this.events
+            .get(eventName)
+            .map(callback => callback(...args));
+    }
+}
+/**
+ * const emitter = new EventEmitter();
+ *
+ * // Subscribe to the onClick event with onClickCallback
+ * function onClickCallback() { return 99 }
+ * const sub = emitter.subscribe('onClick', onClickCallback);
+ *
+ * emitter.emit('onClick'); // [99]
+ * sub.unsubscribe(); // undefined
+ * emitter.emit('onClick'); // []
+ */
