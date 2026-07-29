3cde203bd3f8084bab5e92e6074fe6e5c65e8075
Himanshu Nandal <himanshunandal0103@gmail.com>
2026-07-29 23:05:35 +0530

    Time: 67 ms (36.48%), Space: 54.9 MB (29.2%) - LeetHub

---
+++ 2721-execute-asynchronous-functions-in-parallel/2721-execute-asynchronous-functions-in-parallel.js
@@ -0,0 +1,25 @@
+/**
+ * @param {Array<Function>} functions
+ * @return {Promise<any>}
+ */
+var promiseAll = function(functions) {
+    return new Promise((resolve, reject) => {
+        const results = new Array(functions.length);
+        let resolved = 0;
+
+        functions.forEach((fn, i) => {
+            fn()
+                .then(val => {
+                    results[i] = val;
+                    if (++resolved === functions.length) resolve(results);
+                })
+                .catch(reject); // first rejection wins
+        });
+    });
+};
+
+
+/**
+ * const promise = promiseAll([() => new Promise(res => res(42))])
+ * promise.then(console.log); // [42]
+ */
