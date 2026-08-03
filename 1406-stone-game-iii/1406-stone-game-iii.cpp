599c7f15b6d952b0fb06625791cd4c1e11f32394
Himanshu Nandal <himanshunandal0103@gmail.com>
2026-08-03 13:10:20 +0530

    Time: 17 ms (76.85%), Space: 135.9 MB (88.2%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -1,36 +1,60 @@
 class Solution {
 public:
-    vector<int> dp;
-    int n;
+    string stoneGameIII(vector<int>& stone) {
 
-    int solve(int i, vector<int>& stone) {
+        int n = stone.size();
 
-        if (i >= n)
-            return 0;
+        vector<int> dp(n + 3, 0);
 
-        if (dp[i] != INT_MIN)
-            return dp[i];
+        for (int i = n - 1; i >= 0; i--) {
 
-        int take = 0;
-        int best = INT_MIN;
+            int take = 0;
+            dp[i] = INT_MIN;
 
-        for (int k = 0; k < 3 && i + k < n; k++) {
-            take += stone[i + k];
-            best = max(best, take - solve(i + k + 1, stone));
+            for (int k = 0; k < 3 && i + k < n; k++) {
+
+                take += stone[i + k];
+
+                dp[i] = max(dp[i], take - dp[i + k + 1]);
+            }
         }
 
-        return dp[i] = best;
-    }
-
-    string stoneGameIII(vector<int>& stoneValue) {
-
-        n = stoneValue.size();
-        dp.assign(n, INT_MIN);
-
-        int diff = solve(0, stoneValue);
-
-        if (diff > 0) return "Alice";
-        if (diff < 0) return "Bob";
+        if (dp[0] > 0) return "Alice";
+        if (dp[0] < 0) return "Bob";
         return "Tie";
     }
+
+    // vector<int> dp;
+    // int n;
+
+    // int solve(int i, vector<int>& stone) {
+
+    //     if (i >= n)
+    //         return 0;
+
+    //     if (dp[i] != INT_MIN)
+    //         return dp[i];
+
+    //     int take = 0;
+    //     int best = INT_MIN;
+
+    //     for (int k = 0; k < 3 && i + k < n; k++) {
+    //         take += stone[i + k];
+    //         best = max(best, take - solve(i + k + 1, stone));
+    //     }
+
+    //     return dp[i] = best;
+    // }
+
+    // string stoneGameIII(vector<int>& stoneValue) {
+
+    //     n = stoneValue.size();
+    //     dp.assign(n, INT_MIN);
+
+    //     int diff = solve(0, stoneValue);
+
+    //     if (diff > 0) return "Alice";
+    //     if (diff < 0) return "Bob";
+    //     return "Tie";
+    // }
 };
