2f8df4d3730595daa18360f7670ea6e8b5132dab
Himanshu Nandal <himanshunandal0103@gmail.com>
2026-08-20 18:49:15 +0530

    Time: 0 ms (100%), Space: 62.8 MB (79.27%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,29 +1,47 @@
+// class Solution {
+// public:
+//     int maxArea(vector<int>& height) {
+//         int n = height.size();
+        
+//         stack<int> st;
+//         st.push(n-1);
+//         for(int i=n-2; i>=0; i--){
+//             if(height[st.top()] < height[i]) st.push(i);
+//         }
+
+//         vector<int> poles;
+//         while(!st.empty()){
+//             poles.push_back(st.top());
+//             st.pop();
+//         }
+
+//         int maxi = 0;
+//         for(int i=0; i<n; i++){
+//             for(int j=0; j<poles.size(); j++){
+//                 int w = poles[j] - i;
+//                 int h = min(height[poles[j]] , height[i]);
+//                 maxi = max(maxi, h * w);
+//             }
+//         }
+
+//         return maxi;
+//     }
+// };
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int n = height.size();
-        
-        stack<int> st;
-        st.push(n-1);
-        for(int i=n-2; i>=0; i--){
-            if(height[st.top()] < height[i]) st.push(i);
-        }
-
-        vector<int> poles;
-        while(!st.empty()){
-            poles.push_back(st.top());
-            st.pop();
-        }
-
-        int maxi = 0;
-        for(int i=0; i<n; i++){
-            for(int j=0; j<poles.size(); j++){
-                int w = poles[j] - i;
-                int h = min(height[poles[j]] , height[i]);
-                maxi = max(maxi, h * w);
+        int left=0;
+        int right=height.size()-1;
+        int ans =0;
+        while(left<right){
+            int area = min(height[left],height[right])*(right-left);
+            ans=max(ans,area);
+            if(height[left]<height[right]){
+                left++;
             }
+            else
+            right--;
         }
-
-        return maxi;
+        return ans;
     }
 };
