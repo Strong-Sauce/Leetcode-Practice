1f4d2a45883bb9d423ff7801a67ea46008969cce
Himanshu Nandal <himanshunandal0103@gmail.com>
2026-08-30 11:04:26 +0530

    Time: 1273 ms (74.91%), Space: 0B (100%) - LeetHub

---
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select u.user_id as buyer_id , u.join_date , coalesce(sum(year(o.order_date) = '2019'), 0) as orders_in_2019 
+from Users u
+left join Orders o
+on u.user_id = o.buyer_id
+group by u.user_id , u.join_date;
