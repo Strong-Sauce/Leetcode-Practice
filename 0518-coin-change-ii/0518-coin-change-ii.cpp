b99e8ed7e8baccef139ef4158b277b5900d05590
Himanshu Nandal <himanshunandal0103@gmail.com>
2026-08-31 10:29:05 +0530

    Time: 4 ms (94.2%), Space: 10.1 MB (96.46%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,25 +1,66 @@
-class Solution {
-    // return the total number of comb/ways for t and i,
-    int sol(vector<int>& coins, vector<vector<int>>& dp, int t, int i){
-        if(i < 0 || t < 0) return 0;
-        if(t == 0) return 1;
+// class Solution {
+//     // return the total number of comb/ways for t and i,
+//     int sol(vector<int>& coins, vector<vector<int>>& dp, int t, int i){
+//         if(i < 0 || t < 0) return 0;
+//         if(t == 0) return 1;
         
-        if(dp[t][i] != -1) return dp[t][i];
+//         if(dp[t][i] != -1) return dp[t][i];
 
-        int incl = sol(coins, dp, t-coins[i], i);
-        int excl = sol(coins, dp, t, i-1);
+//         int incl = sol(coins, dp, t-coins[i], i);
+//         int excl = sol(coins, dp, t, i-1);
 
-        int result = incl + excl;
-        return dp[t][i] = result;
-    }
+//         int result = incl + excl;
+//         return dp[t][i] = result;
+//     }
+// public:
+//     int change(int amount, vector<int>& coins) {
+//         sort(coins.begin(), coins.end());
+//         vector<vector<int>> dp(amount+1, vector<int>(coins.size(), -1));
+//         int ans = sol(coins, dp, amount, coins.size()-1);
+//         return ans;
+//     }
+// };
+
+// class Solution {
+// public:
+//     int change(int amount, vector<int>& coins) {
+//         int n = coins.size();
+//         vector<vector<uint32_t>> dp(amount + 1, vector<uint32_t>(n + 1, 0));
+//         // Amount 0 has exactly one way: 
+//         // choose nothing
+//         // amount 0  means target is the same coin that is chosen as in dp below
+//         for(int j = 0; j <= n; j++){
+//             dp[0][j] = 1;
+//         }
+
+//         for(int i = 1; i <= amount; i++){
+//             for(int j = 1; j <= n; j++){
+//                 // Choice 1: don't use current coin
+//                 dp[i][j] = dp[i][j-1];
+//                 // Choice 2: use current coin
+//                 if(i >= coins[j-1]){
+//                     dp[i][j] += dp[i - coins[j-1]][j];
+//                 }
+//             }
+//         }
+
+//         return dp[amount][n];
+//     }
+// };
+
+class Solution {
 public:
     int change(int amount, vector<int>& coins) {
-        sort(coins.begin(), coins.end());
-        vector<vector<int>> dp(amount+1, vector<int>(coins.size(), -1));
-        for(int c=0; c<coins.size(); c++){
-            dp[0][c] = 0;
+        int n = coins.size();
+        vector<uint32_t> dp(amount+1,0);
+        dp[0] = 1;
+        
+        for(int coin : coins){
+            for(int i=coin;i<=amount;i++){
+               dp[i] += dp[i-coin];
+            }
         }
-        int ans = sol(coins, dp, amount, coins.size()-1);
-        return ans;
+
+        return dp[amount];
     }
 };
