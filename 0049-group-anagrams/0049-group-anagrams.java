b0d296a7c03c12b88c77ae626acfffb5cbdf7b33
Himanshu Nandal <himanshunandal0103@gmail.com>
2026-09-02 12:56:36 +0530

    Time: 17 ms (22.1%), Space: 50 MB (25.9%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,18 +1,38 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        int n = strs.length;
+        Map<String, List<String>> mp = new HashMap<>();
 
-        HashMap<String, List<String>> ans = new HashMap<>();
-        for(int i=0;i<n;i++){
-            char[] chars = strs[i].toCharArray(); // i=1 -> eat
-            Arrays.sort(chars); // aet
-            String key = new String(chars); // a e t
-            
-            if(!ans.containsKey(key)) ans.put(key, new ArrayList<>());
-            ans.get(key).add(strs[i]);
+        for (String s : strs) {
+            int[] frq = new int[26];
+
+            for (int i = 0; i < s.length(); i++) 
+                frq[s.charAt(i) - 'a']++;
+
+            StringBuilder key = new StringBuilder();
+
+            for (int x : frq) 
+                key.append(x).append('#');
+
+            mp.computeIfAbsent(key.toString(), k -> new ArrayList<>()).add(s);
         }
-        return new ArrayList<>(ans.values());
+
+        return new ArrayList<>(mp.values());
     }
+
+    // public List<List<String>> groupAnagrams(String[] strs) {
+    //     int n = strs.length;
+
+    //     HashMap<String, List<String>> ans = new HashMap<>();
+    //     for(int i=0;i<n;i++){
+    //         char[] chars = strs[i].toCharArray(); // i=1 -> eat
+    //         Arrays.sort(chars); // aet
+    //         String key = new String(chars); // a e t
+            
+    //         if(!ans.containsKey(key)) ans.put(key, new ArrayList<>());
+    //         ans.get(key).add(strs[i]);
+    //     }
+    //     return new ArrayList<>(ans.values());
+    // }
 
     // public List<List<String>> groupAnagrams(String[] strs) {
     //     int n = strs.length;
