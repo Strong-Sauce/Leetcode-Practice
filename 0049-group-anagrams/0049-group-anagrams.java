0ae140c7f45e400dd07500e26fd7123769e27e86
Himanshu Nandal <himanshunandal0103@gmail.com>
2026-09-02 12:52:29 +0530

    Time: 7 ms (72.77%), Space: 49.9 MB (33.48%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,41 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        int n = strs.length;
+
+        HashMap<String, List<String>> ans = new HashMap<>();
+        for(int i=0;i<n;i++){
+            char[] chars = strs[i].toCharArray(); // i=1 -> eat
+            Arrays.sort(chars); // aet
+            String key = new String(chars); // a e t
+            
+            if(!ans.containsKey(key)) ans.put(key, new ArrayList<>());
+            ans.get(key).add(strs[i]);
+        }
+        return new ArrayList<>(ans.values());
+    }
+
+    // public List<List<String>> groupAnagrams(String[] strs) {
+    //     int n = strs.length;
+
+    //     String[][] s = new String[n][2];
+
+    //     for (int i = 0; i < n; i++) {
+    //         char[] arr = strs[i].toCharArray();
+    //         Arrays.sort(arr);
+
+    //         s[i][0] = new String(arr);
+    //         s[i][1] = String.valueOf(i);
+    //     }
+    //     Arrays.sort(s, (a, b) -> a[0].compareTo(b[0]));
+
+    //     List<List<String>> ans = new ArrayList<>();
+
+    //     for (int i = 0; i < n; i++) {
+    //         if (i == 0 || !s[i][0].equals(s[i - 1][0])) 
+    //             ans.add(new ArrayList<>());
+
+    //         ans.get(ans.size() - 1).add(strs[Integer.parseInt(s[i][1])]);
+    //     }
+    //     return ans;
+    // }
+}
